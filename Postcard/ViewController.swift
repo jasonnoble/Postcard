c01c6bfa3b75658564258a6d206e62f940f0a01b
Jason Noble <github+jasonn@jasonnoble.org>
2014-09-17 10:13:36 -0600

    Added From text field

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var enterNameTextField: UITextField!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -36,6 +38,12 @@ class ViewController: UIViewController {
         enterMessageTextField.resignFirstResponder()
         messageLabel.textColor = UIColor.redColor()
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
     }
 
 }
